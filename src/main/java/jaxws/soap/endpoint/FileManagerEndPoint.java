68c26e2319a18980f29c5594aac3236a2a3582eb
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-16 08:55:48 +0100

    fix error

--- src/main/java/jaxws/soap/endpoint/FileManagerEndPoint.java
+++ src/main/java/jaxws/soap/endpoint/FileManagerEndPoint.java
@@ -2,10 +2,12 @@ package jaxws.soap.endpoint;
 
 import javax.activation.DataHandler;
 import javax.jws.WebParam;
+import javax.jws.WebService;
 
 /**
  * Created by Ghazi Naceur on 15/06/2017.
  */
+@WebService
 public interface FileManagerEndPoint {
 
     void upload(@WebParam(name = "file") DataHandler file);
